236b4f61aef812fec3660619d541a167f1409e51
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2025-11-19 07:18:11 +0100

    fix: check if the active element is an text input

--- common/navigation.js
+++ common/navigation.js
@@ -1,6 +1,6 @@
 function addArrowNavigation(backPageBtn, nextPageBtn) {
     document.addEventListener('keydown', (e) => {
-        if (e.repeat) {
+        if (e.repeat || isInputFocused()) {
             return;
         }
 
@@ -10,4 +10,12 @@ function addArrowNavigation(backPageBtn, nextPageBtn) {
             nextPageBtn?.click();
         }
     });
+}
+
+function isInputFocused() {
+    return document.activeElement &&
+    [
+        'input',
+        'textarea'
+    ].includes(document.activeElement.tagName.toLowerCase());
 }
